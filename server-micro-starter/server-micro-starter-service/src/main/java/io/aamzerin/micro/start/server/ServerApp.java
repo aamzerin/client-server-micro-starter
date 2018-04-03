6ec5646904699fdf05fc18fd3aa5433f2a1b441f
Abdessamad Amzerin <aamzerin@everis.com>
2018-04-03 12:24:20 +0100

    changed config tu embedded mongo database

--- server-micro-starter/server-micro-starter-service/src/main/java/io/aamzerin/micro/start/server/ServerApp.java
+++ server-micro-starter/server-micro-starter-service/src/main/java/io/aamzerin/micro/start/server/ServerApp.java
@@ -1,13 +1,20 @@
 package io.aamzerin.micro.start.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @EnableDiscoveryClient
 @EnableFeignClients("io.aamzerin.micro.**.service")
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableReactiveMongoRepositories
+@AutoConfigureAfter(EmbeddedMongoAutoConfiguration.class)
 public class ServerApp {
 
 	public static void main(String[] args) {
--- server-micro-starter/server-micro-starter-service/src/main/java/io/aamzerin/micro/start/server/repository/DataRepository.java
+++ server-micro-starter/server-micro-starter-service/src/main/java/io/aamzerin/micro/start/server/repository/DataRepository.java
@@ -1,12 +1,10 @@
 package io.aamzerin.micro.start.server.repository;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
 
 import io.aamzerin.micro.start.server.domain.ServerData;
 import reactor.core.publisher.Flux;
 
-@Repository
 public interface DataRepository extends ReactiveCrudRepository<ServerData, String>{
 
 	Flux<ServerData> findByContent(String content);
